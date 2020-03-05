3ab015f2e6549e77ba23ea9ddd81d274fa16aaa4
tbalmeida <tbalmeida@gmail.com>
2020-03-05 22:39:09 +0000

    Functionality done, now working on layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # resources :about,  only: [:index]
   get '/about', to: 'about#index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
