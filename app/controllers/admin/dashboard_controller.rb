1fa6ff3953cfd929485dce9d9cb21fe49f7e617e
tbalmeida <tbalmeida@gmail.com>
2020-03-05 02:38:18 +0000

    Initial setup for basic auth

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PWD"]
+
   def show
   end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV["ADMIN_LOGIN"], password: ENV["ADMIN_PWD"]
 
   def index
     @products = Product.order(id: :desc).all
