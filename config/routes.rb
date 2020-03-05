de27a4c19e6232ca16188f07d0ff0f7bd2178086
tbalmeida <tbalmeida@gmail.com>
2020-03-05 03:14:08 +0000

    Feature: About Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+  # resources :about,  only: [:index]
+  get '/about', to: 'about#index'
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
   end
-
+  
   resources :orders, only: [:create, :show]
 
   namespace :admin do
