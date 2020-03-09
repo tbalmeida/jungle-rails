8a84c74dac09c815ed610851e5b7c6fddda04cba
tbalmeida <tbalmeida@gmail.com>
2020-03-09 22:49:00 +0000

    Model spec: Product

---
+++ spec/models/widget_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Widget, type: :model do
+  describe '#id' do
+  it 'should not exist for new records' do
+    @widget = Widget.new
+    expect(@widget.id).to be_nil
+  end
+
+  it 'should be auto-assigned by AR for saved records' do
+    @widget = Widget.new
+    # we use bang here b/c we want our spec to fail if save fails (due to validations)
+    # we are not testing for successful save so we have to assume it will be successful
+    @widget.save!
+
+    expect(@widget.id).to be_present
+  end
+end
