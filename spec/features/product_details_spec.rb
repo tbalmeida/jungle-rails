d757fb46b5a984940eb31d71d9ee5391f819c6d2
tbalmeida <tbalmeida@gmail.com>
2020-03-10 20:05:26 +0000

    Feature Spec: Product Details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+  require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+    )
+  end
+
+  scenario "Check details of a product" do
+    visit root_path
+
+    find('a.btn.btn-default').click
+    page.has_content?('Description')
+
+    save_screenshot
+  
+  end
+
+end
